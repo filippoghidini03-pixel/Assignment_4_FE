c24fbf7ea02250f7009d663b1ed929ff5195167d
Carlo Cirilli <carlo.cirilli12@gmail.com>
2026-04-17 16:42:30 +0200

    compute2_quadrature bozza (vedere se va etc)

---
+++ compute2_Quadrature.m
@@ -0,0 +1,17 @@
+function C = compute2_Quadrature(F0, DF, x, phi_u)
+    % COMPUTE2_QUADRATURE Prices the call option using numerical integration.
+    % IMPUTS:
+    %   F0         - Forward price
+    %   DF         - Discount factor
+    %   x          - Log-moneyness
+    %   phi_u      - Characteristic function
+    % OUTPUT:
+    %   C          - Call option price
+
+    % We integrate from 0 to infinity and take the real part multiplied by 1/pi
+    f = @(u)exp(-1i * u * x) .* phi_u ./ (u.^2 + 0.25);
+    
+    integral_val = (1/pi) * integral(f, 0, inf, 'RelTol', 1e-8, 'AbsTol', 1e-12);
+
+    C = F0 * DF * (1 - exp(-x/2) * integral_val);
+end
