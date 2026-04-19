62f9ddba79a7eb23561e4ad934097bd5214090de
Carlo Cirilli <carlo.cirilli12@gmail.com>
2026-04-19 17:35:48 +0200

    typo

--- Lewis_FFT_pricer.m
+++ Lewis_FFT_pricer.m
@@ -4,7 +4,7 @@ function [call_prices, x_grid, z_grid] = Lewis_FFT_pricer(phi, F0, B, M, dz, x1,
 integrand = @(xi) phi(-xi - 0.5i) ./ (2 * pi .* (xi.^2 + 0.25));
 
 % Actual Pricing
-[vals, x_grid, z_grid] = FFT_engine(integrand, dz, x1, z1, M);
+[vals, x_grid, z_grid] = FFT_Engine(integrand, dz, x1, z1, M);
 
 % COMMENT: real part taken just to avoid rounding errors made by the pc
 % which could give in output an imaginary part
