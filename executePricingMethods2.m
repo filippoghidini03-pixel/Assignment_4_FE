c916a06dd402f94ad6e95b123e19f154773da01b
Carlo Cirilli <carlo.cirilli12@gmail.com>
2026-04-19 15:31:51 +0200

    forgot this (have to see if it works for point 4)

---
+++ executePricingMethods2.m
@@ -0,0 +1,60 @@
+function [C_quad, C_MC] = executePricingMethods2(x_grid, F0, DF, parameters)
+%Compute Call Prices using Quadrature and Monte Carlo for exercises 3 and 4
+%IMPUTS:
+% x_grid: grid of log-moneyness values
+% F0: forward price
+% DF: discount factor
+% parameters: vectors of parameters [p_plus/alpha, p_minus/sigma, mu/k, eta, dt]
+%OUTPUTS:
+% C_quad: call prices using quadrature
+% C_MC: call prices using Monte Carlo
+N      = length(x_grid);
+C_quad = zeros(1, N);
+C_MC   = zeros(1, N);
+
+if length(parameters) == 3
+    p_plus = parameters(1);
+    p_minus = parameters(2);
+    mu = parameters(3);
+    phi = ChFuncEx3( p_plus, p_minus, mu);
+     for i = 1:N
+        x = x_grid(i);
+        K = F0 * exp(-x);
+        
+        fprintf('--- Pricing for Moneyness x = %.2f%% (Strike K = %.2f) ---\n', x*100, K);
+        
+        % a. Quadrature
+        C_quad(i) = compute2_Quadrature(x, F0, DF, phi);
+        fprintf('a. Quadrature Price:  %.4f\n', C_quad(i));
+        
+        % b. Monte Carlo Simulation
+        C_MC(i) = computeI_MonteCarlo(x, F0, DF, K, p_plus, p_minus, mu);
+        fprintf('c. Monte Carlo Price: %.4f\n', C_MC(i));
+        
+        fprintf('\n');
+    end
+end
+if length(parameters) == 5
+    alpha = parameters(1);
+    sigma = parameters(2);
+    k = parameters(3);
+    eta = parameters(4);
+    dt = parameters(5);
+    phi = Levy_Model_Char_Func(alpha, sigma, k, eta, dt);
+    for i = 1:N
+        x = x_grid(i);
+        K = F0 * exp(-x);
+        fprintf('--- Pricing for Moneyness x = %.2f%% (Strike K = %.2f) ---\n', x*100, K);
+        C_quad(i) = compute2_Quadrature(x, F0, DF, phi);
+        fprintf('a. Quadrature Price:  %.4f\n', C_quad(i));
+        
+        C_MC(i) = compute2_MonteCarlo(x, F0, DF, alpha, sigma, k, eta, dt);
+        fprintf('c. Monte Carlo Price: %.4f\n', C_MC(i));
+        
+        fprintf('\n');
+    end
+end
+
+
+    
+    
